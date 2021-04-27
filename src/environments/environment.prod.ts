d6feac9849aa561c3050a347b1a625a88796caee
deathtracks <antoine.doste@hotmail.fr>
2021-04-27 08:36:59 +0200

    removed the base url witch was incorrect. But still waiting for the true one

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey : "=HBvRe*EI`X=r]@Z!vh*yZcO2)1xC",
+  baseUrl : {
+    //TODO : 'set the right base url
+    base : '',
+    boquette : '/boquette',
+    rotance : '/rotance',
+    channel : '/channel',
+    posts : '/posts',
+    auth : '/auth'
+  }
 };
